a4c06fe487ae202b3b4c38d2636edc40de89476d
Ashmn07 <ashwin.menon202@gmail.com>
2021-07-22 19:29:04 +0530

    Socket fix 1

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express')
 require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT || 8080
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+// const server = require('http').Server(app)
+const io = require('socket.io')(PORT)
 
 require('./models/Document')
 require('./models/DomainModel')
